ad741574db7aaac11170419daf9946527777cf93
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-09-19 20:50:40 -0700

    code #7 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const newTemp = (temp - 32) * (5 / 9);
+  const cel = newTemp.toFixed(1);
+  if(parseInt(cel) == cel){
+    return parseInt(cel)
+  }
+  else{
+  return parseFloat(cel);}
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const newTemp = temp * (9 / 5) + 32;
+  const fahr = newTemp.toFixed(1);
+  if(parseInt(fahr)  == fahr){
+  return parseInt(fahr);
+}
+else{
+  return parseFloat(fahr);}
+  
 };
+
+//Solution more straightforward with Math.round function 
+//wouldn't have to do the parseInt or ParseFloat
 
 // Do not edit below this line
 module.exports = {
